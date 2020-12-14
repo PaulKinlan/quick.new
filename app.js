5d1be8c12e35cb83391b6862a88b6008343f9ea4
Paul Kinlan <paul.kinlan@gmail.com>
2020-12-14 11:43:05 +0000

    remove a comment.

--- app.js
+++ app.js
@@ -1,8 +1,3 @@
-// server.js
-// where your node app starts
-
-// we've started you off with Express (https://expressjs.com/)
-// but feel free to use whatever libraries or frameworks you'd like through `package.json`.
 import compression from "compression";
 import express from "express";
 
